94fd0d23f96f6b5506f8a349f42b392abef77fae
stakamura <shou7sc@yahoo.co.jp>
2017-10-04 02:19:48 +0000

    add-06

--- initial-intall/initial-package-install.sh
+++ initial-intall/initial-package-install.sh
@@ -1,10 +1,14 @@
 #/bin/bash
 
 sudo apt-get update
-sudo apt-get --yes install dstat tree expect unzip
+sudo apt-get --yes install "dstat" "tree" "expect" "unzip" "mysql-common" "libmysqlclient20"
 
+#################################
 # reference
-# expect
-# https://qiita.com/dogyear/items/e58ddab9a49bf82ed43f
-# https://qiita.com/ine1127/items/cd6bc91174635016db9b
-
+#################################
+# - expect
+#   - https://qiita.com/dogyear/items/e58ddab9a49bf82ed43f
+#   - https://qiita.com/ine1127/items/cd6bc91174635016db9b
+#
+#
+#
