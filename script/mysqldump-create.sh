ccd9e4eeed31bdbfebd51562a58bab16b277745b
ubuntu-Xenial-16.05 <ubuntu-Xenial-01@example.com>
2017-10-12 10:18:39 +0000

    add-mysqldump-create

--- script/mysqldump-create.sh
+++ script/mysqldump-create.sh
@@ -8,13 +8,13 @@ rootpass="****"
 
 ########################
 
-mysqldump -u root -p[@rootpass] –all-databases > mysql-backup-def.sql &&
+mysqldump -u root -p{$rootpass} –all-databases > mysql-backup-def.sql &&
 
 ls mysql-backup-def.sql
 
 if [  $? = 0  ]; then
   echo "正常にダンプファイルが作成されました"
-  echo `ls -al mysql-backup-def.sql`
+  echo `ll -al mysql-backup-def.sql`
 else
   echo "正常にダンプファイルが作成されました"
 fi
