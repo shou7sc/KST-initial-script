7155fa76f69061cd3ed581b9df3845e8c800d91e
ubuntu-Xenial-16.05 <ubuntu-Xenial-01@example.com>
2017-10-12 09:57:07 +0000

    rest-script add

---
+++ script/rest-app-for-python.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -ex
+
+if [ -f /var/lib/mysql/isucon7-image-slow.log ]; then
+    sudo mv /var/lib/mysql/isucon7-image-slow.log /var/lib/mysql/isucon7-image-slow.log.$(date "+%Y%m%d_%H%M%S")
+fi
+if [ -f /var/log/nginx/access-log ]; then
+    sudo mv /var/log/nginx/access-log /var/log/nginx/access-log.$(date "+%Y%m%d_%H%M%S")
+fi
+
+sudo systemctl restart mysql.service
+sudo systemctl restart isutar.python.service
+sudo systemctl restart isuda.python.service
+sudo systemctl restart nginx.service
