13e8f4b450fa5307934ceb8bab19ddea2c013727
ubuntu-Xenial-16.05 <ubuntu-Xenial-01@example.com>
2017-10-13 05:26:16 +0000

    add initial-setting/root-dir-create.sh

---
+++ initial-setting/root-dir-create.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "`id | grep root`" = "" ]
+then
+    echo "root de execute sitene!"
+    exit 1
+fi
+
+cd /root &&
+mkdir analysis &&
+cd analysis &&
+mkdir dstat access-analysys sql-analysys &&
+cd ../ &&
+mkdir backup-def &&
+cd backup-def &&
+mkdir AP DB WEB &&
+cd ../
+tree -L 2 
