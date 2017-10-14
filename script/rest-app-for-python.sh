cd944497ab0d4735cb93ba2d60e8ef122948a3e3
shou7sc <shou7sc@yahoo.co.jp>
2017-10-14 10:20:54 +0900

    gitadd rest app

--- script/rest-app-for-python.sh
+++ script/rest-app-for-python.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -ex
 
-if [ -f /var/lib/mysql/isucon7-image-slow.log ]; then
-    sudo mv /var/lib/mysql/isucon-slow.log /root/analysis/sql-analysys/isucon-slow.log.$(date "+%Y%m%d_%H%M%S")
+if [ -f /var/lib/mysql/isucon-slow.log ]; then
+    sudo mv /var/lib/mysql/isucon-slow.log /root/analysis/sql-analysys/log/isucon-slow.log.$(date "+%Y%m%d_%H%M%S")
 fi
 if [ -f /var/log/nginx/access-log ]; then
-    sudo mv /var/log/nginx/access-log /root/analysis/access-analysys/access-log.$(date "+%Y%m%d_%H%M%S")
+    sudo mv /var/log/nginx/access-log /root/analysis/access-analysys/log/access-log.$(date "+%Y%m%d_%H%M%S")
 fi
 
-sudo systemctl restart mysql.service
-sudo systemctl restart isutar.python.service
-sudo systemctl restart isuda.python.service
+sudo systemctl restart mysql.service &&
+sudo systemctl restart isutar.python.service &&
+sudo systemctl restart isuda.python.service && 
 sudo systemctl restart nginx.service
