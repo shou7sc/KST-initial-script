96410ee2e091c6043e68308e2a11b38439e64cc0
shou7sc <shou7sc@yahoo.co.jp>
2017-10-14 11:06:53 +0900

    script-change

--- script/rest-app-for-ruby.sh
+++ script/rest-app-for-ruby.sh
@@ -2,10 +2,10 @@
 set -ex
 
 if [ -f /var/lib/mysql/isucon-slow.log ]; then
-    sudo mv /var/lib/mysql/isucon-slow.log /root/analysis/sql-analysys/log/isucon-slow.log.$(date "+%Y%m%d_%H%M%S")
+    sudo mv /var/lib/mysql/isucon-slow.log /root/analysis/sql-analysys/01-log/isucon-slow.log.$(date "+%Y%m%d_%H%M%S")
 fi
 if [ -f /var/log/nginx/access-log ]; then
-    sudo mv /var/log/nginx/access-log /root/analysis/access-analysys/log/access-log.$(date "+%Y%m%d_%H%M%S")
+    sudo mv /var/log/nginx/access-log /root/analysis/access-analysys/01-log/access-log.$(date "+%Y%m%d_%H%M%S")
 fi
 
 sudo systemctl restart mysql.service &&
