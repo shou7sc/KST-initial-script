fa02497e3bcde9bc46148dfd058b82104dd41db9
shou7sc <shou7sc@yahoo.co.jp>
2017-10-14 10:16:19 +0900

    initial-setting/root-dir-create.sh

--- initial-setting/root-dir-create.sh
+++ initial-setting/root-dir-create.sh
@@ -10,9 +10,13 @@ cd /root &&
 mkdir analysis &&
 cd analysis &&
 mkdir dstat access-analysys sql-analysys &&
+mkdir ./dstat/dstat-res-analysis &&
+mkdir ./access-analysys/acc-res-analysis &&
+mkdir ./sql-analysys/sql-res-analysis &&
+mkdir ./dstat/log ./access-analysys/log ./sql-analysys/log &&
 cd ../ &&
 mkdir backup-def &&
 cd backup-def &&
 mkdir AP DB WEB &&
 cd ../ &&
-tree -L 2 
+tree -L 4
