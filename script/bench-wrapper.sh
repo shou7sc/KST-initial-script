2fac9be19cb2866c3d6a9d0b7232d7ec33f11bc5
shou7sc <shou7sc@yahoo.co.jp>
2017-10-15 02:33:26 +0900

    perform-add

--- script/bench-wrapper.sh
+++ script/bench-wrapper.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+
+./top-info-roop.sh
---
+++ script/top-info-roop.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+################################
+#
+# TOP CPU INFO 
+#
+#################################
+
+top d1 -c -b -d 1 -n 120 > /root/analysis/cpu-info/02-bench-log/top-cpu-info.log.`date '+%m%d-%H%M'` &
+
