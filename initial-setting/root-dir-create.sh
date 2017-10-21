1d9ae85906fca2ca969d5c36dc855ff4f8c5ddb6
isucon-master <isucon-master@example.co.jp>
2017-10-21 22:32:46 +0900

    change

--- initial-setting/root-dir-create.sh
+++ initial-setting/root-dir-create.sh
@@ -14,8 +14,6 @@ mkdir ./dstat/02-dstat-res-analysis &&
 mkdir ./access-analysys/02-acc-res-analysis &&
 mkdir ./sql-analysys/02-sql-res-analysis &&
 mkdir ./dstat/01-log ./access-analysys/01-log ./sql-analysys/01-log &&
-mkdir ./cpu-info/01-normal-log ./cpu-info/02-bench-log &&
-mkdir ./mem-info/01-normal-log ./mem-info/02-bench-log &&
 cd ../ &&
 mkdir backup &&
 cd backup &&
--- script/bench-wrapper.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./top-info-roop.sh
